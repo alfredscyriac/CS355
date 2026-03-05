1f0158134018e27f6f788efa19520637e9334198
Alfred <alfredscyriac@gmail.com>
2026-03-05 18:26:20 -0500

    01 solution

--- Assessments/01-asynchronous-programming/01/01-n-files-asynchronously.js
+++ Assessments/01-asynchronous-programming/01/01-n-files-asynchronously.js
@@ -1,11 +1,22 @@
-/*
-=======================
-01-n-files-asynchronously.js
-=======================
-Student ID:
-Comment (Required):
-
-=======================
-*/
 const fs = require("fs");
-const n = 5;	//input size 0 < n < 100
+const n = 5;	// input size 0 < n < 100
+
+const outputDirectory = "./output";
+const data = "Data-1";  
+let written = 0; 
+
+for(let i = 0; i < 100; i++){
+    let filename = `${i.toString().padStart(2,"0")}-output.txt`; 
+    let path = `${outputDirectory}/${filename}`; 
+    fs.writeFile(path, data, after_write); 
+    function after_write(err){
+        if(err){
+            throw err; 
+        }
+        written += 1;
+        console.log(`Finished Writing File ${file}`);
+        if(written == n) {
+            console.log("Writing Complete");
+        }
+    }
+}
