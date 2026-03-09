e391a3fdafe797c50afbdcc45b55692ace7c34b3
Alfred <alfredscyriac@gmail.com>
2026-03-09 17:15:52 -0400

    04 solution w output

--- Assessments/01-asynchronous-programming/03/03-personal-hosts-file.js
+++ Assessments/01-asynchronous-programming/03/03-personal-hosts-file.js
@@ -19,19 +19,23 @@ const output_file = "./output/hosts.txt";
 fs.readFile(input_file, "utf8", after_read);
 
 function after_read(err, data){
-    let domains = data.split("\n");
-    let results = []; 
+    let domains = data.split("\n").filter(d => d.trim() !== "");
+    let results = [];
 
     for(let i = 0; i < domains.length; i++){
-        resolve(i, domains, results);  
+        resolve(i, domains, results);
     }
 }
 function resolve(i, domains, results){
     dns.resolve(domains[i], after_resolution);
-    function after_resolution(err, records){ 
-        results.push(`${records[0]}\t${domains[i]}`); 
+    function after_resolution(err, records){
+        if(err){
+            results.push(`ERROR\t${domains[i]}`);
+        } else {
+            results.push(`${records[0]}\t${domains[i]}`);
+        }
         if(results.length == domains.length){
-            fs.writeFile(output_file, results.join("\n"), () => console.log("Finished Writing")); 
+            fs.writeFile(output_file, results.join("\n"), () => console.log("Finished Writing"));
         }
     }
 }
--- Assessments/01-asynchronous-programming/04/04-four-synchronous-tasks.js
+++ Assessments/01-asynchronous-programming/04/04-four-synchronous-tasks.js
@@ -1,14 +1,33 @@
 /*
-=======================
-04-four-synchronous-tasks.js
-=======================
-Student ID:
-Comment (Required):
+Four synchronous tasks
+- Allowed Node.js API Functions: fs.readFile(), zlib.inflate(), dns.resolve(), fs.writeFile()
+- Input: domain.deflated
+- Output: File: ip_address.txt
 
-=======================
+Write a program that:
+1. Reads domain.deflated (Use {encoding:null} ), 
+2. Decompresses the contents using zlib.inflate(), covert the resulting buffer to a string using .toString("utf8") the decompressed data will be a valid domain that resolves to a single IP address.
+3. Using dns.resolve() convert the domain into an IP address.
+4. Remove the array wrapper and write the IP address to a file ip_address.txt
 */
 const fs = require("fs");
 const dns = require("dns");
 const zlib = require("zlib");
 const input_file = "./input/domain.deflated";
 const output_file = "./output/ip_address.txt";
+
+fs.readFile(input_file, after_read); 
+
+function after_read(err, compressed_buffer){
+    zlib.inflate(compressed_buffer, after_decompress); 
+}
+
+function after_decompress(err, buffer){
+    let domain = buffer.toString("utf8"); 
+    dns.resolve(domain, (err, records) => after_resolution(err, records, domain)); 
+}
+
+function after_resolution(err, records, domain){
+    let results = `${records[0]}\t${domain}`; 
+    fs.writeFile(output_file, results, () => console.log("Writing Complete")); 
+}
