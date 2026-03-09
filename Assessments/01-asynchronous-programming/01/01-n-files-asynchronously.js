b2c7220f386317dbcfce4b811bad2b1988668fa5
Alfred <alfredscyriac@gmail.com>
2026-03-08 23:22:27 -0400

    fixed solutions with output and notes

--- Assessments/01-asynchronous-programming/01/01-n-files-asynchronously.js
+++ Assessments/01-asynchronous-programming/01/01-n-files-asynchronously.js
@@ -1,22 +1,32 @@
+/* 
+Writing N Files Asynchronously
+- Allowed Node.js API Functions: fs.writeFile()
+- Input: Integer n  (0 < n < 100)
+- Output: n Files
+
+Create a program that asynchronously write n files 01-output.txt up to 99-output.txt to a subdirectory output.  Each file should contain the text "Data-1".  After each successful write, print a message to console with the filename.  After all n files are finished writing, print an additional message Writing Complete.  Run the script a few times, the filenames printed should almost never be in order.
+*/
+
 const fs = require("fs");
 const n = 5;	// input size 0 < n < 100
 
-const outputDirectory = "./output";
-const data = "Data-1";  
+const outputDirectory = "./output"; 
+const data = "Data-1"; 
+
 let written = 0; 
 
-for(let i = 0; i < 100; i++){
-    let filename = `${i.toString().padStart(2,"0")}-output.txt`; 
-    let path = `${outputDirectory}/${filename}`; 
-    fs.writeFile(path, data, after_write); 
-    function after_write(err){
-        if(err){
-            throw err; 
-        }
-        written += 1;
-        console.log(`Finished Writing File ${file}`);
-        if(written == n) {
-            console.log("Writing Complete");
+for(let i = 0; i < n; i++){ 
+    let file = `${i.toString().padStart(2,"0")}-output.txt`; 
+    let path = `${outputDirectory}/${file}`; 
+
+    fs.writeFile(path, data, afterWrite); 
+
+    function afterWrite(){
+        console.log(`Finished Writing File ${file}`); 
+
+        written += 1; 
+        if(written == n){
+            console.log("Writing Complete"); 
         }
     }
 }
--- Assessments/01-asynchronous-programming/02/02-n-files-synchronously.js
+++ Assessments/01-asynchronous-programming/02/02-n-files-synchronously.js
@@ -1,25 +1,39 @@
+/*
+Writing N Files Synchronously
+- Allowed Node.js API Functions: fs.writeFile()
+- Input: Integer n  (0 < n < 100)
+- Output: n Files
+
+Create a program that  synchronously write n files 01-output.txt up to 99-output.txt to a subdirectory output.  Each file should contain the text "Data-2".   After each successful fs.writeFile() print a message to console with the filename.  After all n files are finished, print an additional message Writing Complete Run the script a few times, the filenames printed should always be in order.  
+*/
+
 const fs = require("fs");
-const n = 10; // input size 0 < n < 100
-const output_dir = "./output";
-const data = "Data-2";
+const n = 5; // input size 0 < n < 100
 
-let i = 1;
-let file = `${i.toString().padStart(2, "0")}-output.txt`;
+const outputDirectory = "./output"; 
+const data = "Data-2"; 
 
-writeSync();
+// If you ever see synchronously with regard to asynchronous programming you should automatically be thinking recursively
+// In this case write the first file and then inside the callback you make a recursive call to write the second file and so on 
+// The reason for that is because this way you can only start writing the second file after the first one finishes which mimics blocking style of programming
+// Closures aren't required for this type of scynchronous behavior 
 
-function writeSync() {
-  let path = `${output_dir}/${file}`;
-  fs.writeFile(path, data, after_write);
+writeSync(1); 
+function writeSync(i){
+    let file = `${i.toString().padStart(2,"0")}-output.txt`; 
+    let path = `${outputDirectory}/${file}`; 
+    
+    fs.writeFile(path, data, (err) => afterWrite(i,file)); 
 }
 
-function after_write(err) {
-  console.log(`Finished Writing: ${file}`);
-  if (i == n) {
-    console.log("Writing Complete");
-  } else {
-    i++;
-    file = `${i.toString().padStart(2, "0")}-output.txt`;
-    writeSync();
-  }
+function afterWrite(i, file){
+    console.log(`Finished Writing File: ${file}`); 
+    if(i == n){
+        console.log("Writing Complete"); 
+    }
+    else {
+        writeSync(i+1);   
+    } 
 }
+
+
